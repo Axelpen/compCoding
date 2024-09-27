039178a0a73722149763f8dc08f6eb889ca29a42
Axelpen <alexander.penck@ncad.de>
2024-09-27 21:20:40 +0200

    431A - Black Square

--- 431A/Black_Square.cpp
+++ 431A/Black_Square.cpp
@@ -1,10 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    int a1,a2,a3,a4,input;
 
-    cin >> a1 >> a2 >> a3 >> a4;
-    
+int main()
+{
+    int a1, a2, a3, a4, cal = 0;
 
+    string str;
+    cin >> a1 >> a2 >> a3 >> a4>> str;
+    for(int i =0;i < str.size();i++)
+    {
+        switch (str[i])
+        {
+        case '4':
+            cal += a4;
+            break;
+        case '3':
+            cal += a3;
+            break;
+        case '2':
+            cal += a2;
+            break;
+        case '1':
+            cal += a1;
+            break;
+        default:
+            break;
+        }
 
+    }
+    cout << cal;
+    return 0;
 }
